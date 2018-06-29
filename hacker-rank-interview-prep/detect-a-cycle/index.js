dc9caf23aa5502e115715354d3754c47abc789ce
Ahmed El Senousi <elsenousi@elsenousi-macbookair.roam.corp.google.com>
2018-06-29 13:59:49 +0100

    detect-a-cycle

---
+++ hacker-rank-interview-prep/detect-a-cycle/index.js
@@ -0,0 +1,53 @@
+function hasCycle(node) {
+  const set = new Set();
+  while(node) {
+    if(set.has(node)) return true;
+    set.add(node);
+    node = node.next;
+  }
+  return false;
+}
+
+describe('detect a cycle', function() {
+  const assert = require('assert');
+
+  class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+
+    add(data) {
+      let current = this;
+      while(current.next !== null) {
+        current = current.next;
+      }
+      current.next = new Node(data);
+      return this;
+    }
+
+    loopBack(point) {
+      let current = this;
+      let pointNode;
+      while(current.next !== null) {
+        if (current.data === point) {
+          pointNode = current;
+        }
+        current = current.next;
+      }
+      current.next = pointNode;
+      return this;
+    }
+  }
+
+  it('should return false', function() {
+    const node = new Node(0).add(1).add(2).add(3);
+    assert.equal(hasCycle(node), false);
+  });
+
+  it('should return true', function() {
+    const node = new Node(0).add(1).add(2).add(3).loopBack(1);
+    assert.equal(hasCycle(node), true);
+  });
+});
+
